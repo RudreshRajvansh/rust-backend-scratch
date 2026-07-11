08b672919478a3c19ee3235fd18a85d278a5f2fe
Rudresh Rajvansh <itsrudreshhere@gmail.com>
2026-07-11 17:55:50 +0000

    Fixed the raw error formatting

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,14 @@ fn create_socket() -> (bool,i32){
         let protocol = 0;
         let response = libc::socket(domain, socket_type, protocol);
         if response < 0 {
-            println!("Error : Socket not created - {}",std::io::Error::last_os_error().raw_os_error());
+            println!("Error : Socket not created - {:?}",std::io::Error::last_os_error().raw_os_error());
             return (false,response);
         }
         else {
             //enable non-blocking to make sure 
-            let status_NB = enable_nonblocking(response);
-            let status_SR = enable_reuseaddr(response);
-            if status_NB && status_SR {
+            let status_nb = enable_nonblocking(response);
+            let status_sr = enable_reuseaddr(response);
+            if status_nb && status_sr {
             return (true,response);
             }else{
                 return (false,response);
@@ -37,14 +37,14 @@ fn enable_nonblocking(socket : i32) -> bool{
         let get_operation = libc::F_GETFL;
         let flags = libc::fcntl(socket, get_operation, 0);
         if flags < 0 {
-            println!("Error : NON-BLOCKING Flag not Applied - {}",std::io::Error::last_os_error().raw_os_error());
+            println!("Error : NON-BLOCKING Flag not Applied - {:?}",std::io::Error::last_os_error().raw_os_error());
             return false;
         }
         //setting flags on socket
         let set_operation = libc::F_SETFL;
         let response = libc::fcntl(socket, set_operation, flags | libc::O_NONBLOCK);
         if response < 0 {
-            println!("Error : NON-BLOCKING Flag not Applied - {}",std::io::Error::last_os_error().raw_os_error());
+            println!("Error : NON-BLOCKING Flag not Applied - {:?}",std::io::Error::last_os_error().raw_os_error());
             return false;
         }
         return true;
@@ -60,7 +60,7 @@ fn enable_reuseaddr(socket : i32) -> bool{
         let length = std::mem::size_of::<i32>() as libc::socklen_t;
         let response = libc::setsockopt(socket, level, option, &status as *const i32 as *const libc::c_void, length);
         if response < 0 {
-            println!("Error : REUSEADDR not Applied - {}",std::io::Error::last_os_error().raw_os_error());
+            println!("Error : REUSEADDR not Applied - {:?}",std::io::Error::last_os_error().raw_os_error());
             return false;
         }
         return true;
@@ -100,17 +100,17 @@ fn epoll_creation(socket: i32) -> i32{
         // creation of a epoll fd 
         let epoll_fd = libc::epoll_create1(0); // 0 -> flags 
         if epoll_fd < 0 {
-            println!("Error : Epoll Error - {}",std::io::Error::last_os_error().raw_os_error());
+            println!("Error : Epoll Error - {:?}",std::io::Error::last_os_error().raw_os_error());
             return -1;
         }
         // addition of socket to epoll fd
-        let mut clieve : libc::epoll_event = std::mem::zeroed();// dont know why zero eveyrthinme asys struct istnace created 
+        let mut clieve : libc::epoll_event = std::mem::zeroed();
         clieve.events = libc::EPOLLIN as u32;
         clieve.u64 = socket as u64;
 
         let status = libc::epoll_ctl(epoll_fd, libc::EPOLL_CTL_ADD, socket, &mut clieve);    
         if status < 0{
-            println!("Error : Epoll Error - {}",std::io::Error::last_os_error().raw_os_error());
+            println!("Error : Epoll Error - {:?}",std::io::Error::last_os_error().raw_os_error());
             return -1;
         }
         return epoll_fd;
@@ -121,15 +121,15 @@ fn accepts(socket: i32, sockep: i32) -> i32{
     unsafe{
         let accept_fd = libc::accept(socket, std::ptr::null_mut(), std::ptr::null_mut());
         // adding accept fd to epoll
-        let statusNB = enable_nonblocking(accept_fd);
-        if statusNB{
+        let status_nb = enable_nonblocking(accept_fd);
+        if status_nb{
             let mut client_event : libc::epoll_event = std::mem::zeroed();
             client_event.events = libc::EPOLLIN as u32;
             client_event.u64 = accept_fd as u64;
 
             let status = libc::epoll_ctl(sockep, libc::EPOLL_CTL_ADD, accept_fd, &mut client_event);
             if status < 0{
-                println!("Error : Epoll Error - {}",std::io::Error::last_os_error().raw_os_error());
+                println!("Error : Epoll Error - {:?}",std::io::Error::last_os_error().raw_os_error());
                 return -1;
             }else{
                 return accept_fd;
@@ -145,7 +145,7 @@ fn readandwrite(epollfd : i32, efd : i32) -> i32{
         let mut buffer = [0u8;1024];
         let status = libc::read(efd, buffer.as_mut_ptr() as *mut libc::c_void, 1024);
         if status < 0{
-            println!("Error : Read Error - {}",std::io::Error::last_os_error().raw_os_error());
+            println!("Error : Read Error - {:?}",std::io::Error::last_os_error().raw_os_error());
             return -1;
         }else if status == 0{
             println!("Connection Closed");
@@ -155,10 +155,10 @@ fn readandwrite(epollfd : i32, efd : i32) -> i32{
         }else{
             let write_status = libc::write(efd, buffer.as_ptr() as *const libc::c_void, status as usize);
             if write_status < 0{
-                println!("Error : Read Error - {}",std::io::Error::last_os_error().raw_os_error());
+                println!("Error : Write Error - {:?}",std::io::Error::last_os_error().raw_os_error());
                 return -1;
             }else{
-                println!("Connection Served!!");
+                println!("Connection Served!! {}", status);
                 return 1;
             }
         }
@@ -169,7 +169,7 @@ fn accept_epoll(socket : i32) -> i32{
     unsafe{
         let efd = epoll_creation(socket);
         if efd < 0{
-            println!("Error : Epoll Error - {}",std::io::Error::last_os_error().raw_os_error());
+            println!("Error : Epoll Error - {:?}",std::io::Error::last_os_error().raw_os_error());
             return -1;
         }
         let mut events: [libc::epoll_event; 64] = std::mem::zeroed();
@@ -179,12 +179,12 @@ fn accept_epoll(socket : i32) -> i32{
                 if events[i as usize].u64 == socket as u64 {
                     let accept_status = accepts(socket, efd);
                     if accept_status < 0{
-                        println!("Error : Client Accept Error - {}",std::io::Error::last_os_error().raw_os_error());
+                        println!("Error : Client Accept Error - {:?}",std::io::Error::last_os_error().raw_os_error());
                     }
                 }else{
                     let readstatus = readandwrite(efd, events[i as usize].u64 as i32);
                     if readstatus < 0 {
-                          println!("Error : Client Read Error - {}",std::io::Error::last_os_error().raw_os_error());
+                          println!("Error : Client Read Error - {:?}",std::io::Error::last_os_error().raw_os_error());
                     }
                 }
             }
@@ -193,25 +193,24 @@ fn accept_epoll(socket : i32) -> i32{
 }
 
 fn main(){
-    unsafe{
     let (status,socket) = create_socket();
     if status{
         let bind_status = bind_socket(socket);
         if bind_status{
             let listen_status = listen_socket(socket);
             if listen_status {
+                println!("Server Active: Listening on localhost:8080");
                 let accept_epoll_status = accept_epoll(socket);
                 if accept_epoll_status < 0 {
-                     println!("Error : Client Accept Error - {}",std::io::Error::last_os_error().raw_os_error());
+                     println!("Error : Client Accept Error - {:?}",std::io::Error::last_os_error().raw_os_error());
                 }
             }else{
-                println!("Error : Status Change Error - {}",std::io::Error::last_os_error().raw_os_error());
+                println!("Error : Status Change Error - {:?}",std::io::Error::last_os_error().raw_os_error());
             }
         }else{
-            println!("Error : Binding Error - {}",std::io::Error::last_os_error().raw_os_error());
+            println!("Error : Binding Error - {:?}",std::io::Error::last_os_error().raw_os_error());
         }
     }else{
-        println!("Error : Server Error - {}",std::io::Error::last_os_error().raw_os_error());
+        println!("Error : Server Error - {:?}",std::io::Error::last_os_error().raw_os_error());
     }
-}
 }
