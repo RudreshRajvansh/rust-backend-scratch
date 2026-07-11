ca827fde35cec2bb2e13023842c0859a949e675b
Rudresh Rajvansh <itsrudreshhere@gmail.com>
2026-07-11 06:16:09 +0000

    Created a Non-blocking socket server next step is adding proper multi client echo current randw fxn is locking thread in loop ...

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+use libc;
+
+
+fn randw(aa : i32){
+    unsafe{
+    let mut buf = [0u8; 1024];
+    let mut rr = libc::read(aa, buf.as_mut_ptr() as *mut libc::c_void, 1024);
+    while rr>0 {
+        if rr > 0 {
+            println!("Read Bytes : {}",String::from_utf8_lossy(&buf[0..rr as usize]));
+                let ww = libc::write(aa,buf.as_ptr() as *mut libc::c_void,rr as usize);
+                if ww > 0 {
+                    println!("Bytes echoed : {}",ww);
+                }else{
+                    println!("Error on write, {}",std::io::Error::last_os_error());
+                    break;
+                }
+        }else{
+            println!("Error on read or empty read, {}",std::io::Error::last_os_error());
+            break;
+        }
+        rr = libc::read(aa, buf.as_mut_ptr() as *mut libc::c_void, 1024);
+}
+}
+}
+fn main() {
+    unsafe{
+        let fd = libc::socket(libc::AF_INET, libc::SOCK_STREAM,0);
+        if fd < 0 {
+            println!("Socket error : {}",std::io::Error::last_os_error());
+        }else{
+            println!("Socket created successfully with fd: {}",fd);
+            let opt: i32 = 1;
+            let set = libc::setsockopt(fd, libc::SOL_SOCKET,libc::SO_REUSEADDR,&opt as *const i32 as *const libc::c_void, std::mem::size_of::<i32>() as libc::socklen_t);
+            if set == 0 {
+                println!("Success reuse enabled");
+            }else{
+                println!("Error occured : {}",std::io::Error::last_os_error());
+            }
+            let mut myaddr : libc::sockaddr_in = std::mem::zeroed();
+                myaddr.sin_family = libc::AF_INET as u16;
+                myaddr.sin_port = libc::htons(8080);
+                myaddr.sin_addr = libc::in_addr{ s_addr: libc::htonl(libc::INADDR_ANY) };
+
+            let bb = libc::bind(fd, &myaddr as *const libc::sockaddr_in as *const libc::sockaddr, std::mem::size_of::<libc::sockaddr_in>() as libc::socklen_t);
+                if bb == 0 {
+                    println!("Bind successful");
+                    let ll = libc::listen(fd, 1024);
+                    if ll == 0 {
+                        println!("Listening on port 8080");
+                        let mut ev : libc::epoll_event = std::mem::zeroed();
+                        ev.events = libc::EPOLLIN as u32;
+                        ev.u64 = fd as u64;
+                        let flags = libc::fcntl(fd, libc::F_GETFL, 0);
+                        let _dd = libc::fcntl(fd, libc::F_SETFL, flags | libc::O_NONBLOCK);
+                        let ep = libc::epoll_create1(0);
+                        let oo = libc::epoll_ctl(ep,libc::EPOLL_CTL_ADD,fd, &mut ev);
+                        let mut events: [libc::epoll_event; 64] = std::mem::zeroed();
+                        loop{
+                        let ew = libc::epoll_wait(ep,events.as_mut_ptr(),64,-1);
+                        for i in 0..ew{
+                            if events[i as usize].u64 == fd as u64 {
+                            let aa = libc::accept(fd,std::ptr::null_mut(),std::ptr::null_mut());
+                             let cflags = libc::fcntl(aa, libc::F_GETFL, 0);
+                             let _cdd = libc::fcntl(aa, libc::F_SETFL, flags | libc::O_NONBLOCK);
+                             let mut cev: libc::epoll_event = std::mem::zeroed();
+                            cev.events = libc::EPOLLIN as u32;
+                            cev.u64 = aa as u64; 
+                            libc::epoll_ctl(ep,libc::EPOLL_CTL_ADD,aa, &mut cev);
+                            println!("A client connected! {}",aa);
+                            }
+                        }
+                    }
+                        // //now we use accept to accept connections
+                        // let mut aa = libc::accept(fd,std::ptr::null_mut(),std::ptr::null_mut());
+                        //    // while aa >= -1 && std::io::Error::last_os_error() == std::io::Error::from_raw_os_error(libc::EAGAIN) {
+                        //    loop{
+                        //     if aa == -1 && std::io::Error::last_os_error().raw_os_error() != Some(libc::EWOULDBLOCK ){
+                        //         println!("Accept error : {}",std::io::Error::last_os_error());
+                        //     } else if aa > 0 {
+                        //             println!("Connection accepted with fd: {}",aa);
+                        //             randw(aa);
+                        //             libc::close(aa);
+                        //             println!("Connection closed! Ready for next");
+                        //     }
+                        //     aa = libc::accept(fd,std::ptr::null_mut(),std::ptr::null_mut());
+                        // }
+                    }
+                    else{
+                            println!("Listen error : {}",std::io::Error::last_os_error()); 
+                    }
+                }else{
+                    println!("Bind error : {}",std::io::Error::last_os_error());
+                }
+        }
+    }
+}
+
